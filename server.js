64db2f49d195e87d578266d707afc5484ddeff1c
dracomithril <dracomithril@gmail.com>
2018-10-17 23:36:44 +0200

    add info about version number

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
 const app = express();
+app.get('/app/version', (req, res) => {
+  const version = process.env.npm_package_version;
+  res.send(version);
+});
 app.use('/storybook', serveStatic(join(__dirname, 'storybook-static')));
 app.use(serveStatic(join(__dirname, 'build')));
 
