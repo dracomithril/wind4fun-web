18539b573c6b45c6f771a031229d07ab7c73865d
dracomithril <dracomithril@gmail.com>
2018-10-17 23:22:36 +0200

    enable storybook on heroku

--- server.js
+++ server.js
@@ -9,14 +9,10 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
 const app = express();
+app.use('/storybook', serveStatic(join(__dirname, 'storybook-static')));
 app.use(serveStatic(join(__dirname, 'build')));
-const storybook = express();
-storybook.use(serveStatic(join(__dirname, 'storybook-static')));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   logger.info(`app server started ${port}`);
-});
-storybook.listen(9009, () => {
-  logger.info('storybook server started');
 });
